b15c3be1a36ec3b8ed96981d9515e254429fa7e7
luisgabriel2007 <139251323+luisgabriel2007@users.noreply.github.com>
2023-07-18 09:23:48 -0600

    Add files via upload

---
+++ while.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int numero;
+    cout <<  "Ingrese un numero ";
+    cin >> numero;
+    while(numero <= 100)
+    {
+        cout <<  "Ingrese un numero ";
+        cin >> numero;
+    }
+
+
+    return 0;
+}
